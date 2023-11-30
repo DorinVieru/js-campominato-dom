a40841ef57e42ca0c741bae2965950b23b0626f7
DorinVieru <dorino.vieru@gmail.com>
2023-11-30 13:44:40 +0100

    [UPDATE] Create file html, css, js & img folder

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// FUNZIONE CHE CREA LA SINGOLA CASELLA DELLA GRIGLIA
+function createCell(num) {
+    const element = document.createElement("div");
+    element.classList.add("square");
+
+    element.innerText = num;
+
+    return element;
+}
+
+// RECUPERO L'ELEMENTO CHE CONTIENE LA GRIGLIA
+const grid = document.getElementById("grid");
+
+// FUNZIONE CHE CONSENTE AL CLICK SUL BOTTONE PLAY DI FAR COMPARIRE LA GRIGLIA DI GIOCO
+function createNewGame() {
+    document.getElementById("grid").innerHTML = "";
+
+    // ESEGUO UN FOR
+    for (let i = 0; i < 100; i++) {
+        // CREO LA CELLA
+        let square = createCell(i + 1);
+
+        
+
+        // APPENDO LA CELLA ALLA GRIGLIA
+        grid.appendChild(square);
+    }
+}
+
+// CREO EVENTO IN ASCOLTO CHE CONSENTE DI CLICCARE IL BOTTONE E FAR PARTIRE IL GIOCO
+document.getElementById("play").addEventListener("click", function () {
+    createNewGame();
+})
